b13ca149fd5ba96f830a0bbc8dc56476d4fd96a1
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2026-02-04 19:34:08 +0200

    refactor: update settings and config imports
    - Import `settings` from `django.conf`
    - Remove wildcard import from `config` module

--- doccoon_backend/config/urls.py
+++ doccoon_backend/config/urls.py
@@ -1,8 +1,8 @@
+from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from config import *
 from doccoon.views.home import (
     HealthApiView,
     HealthLiveApiView,
